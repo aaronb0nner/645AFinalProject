1ec306093558fce6a430d72936cff82313e1fa05
aaronb0nner <aaronbonner00@gmail.com>
2025-03-18 19:25:38 -0400

    Updated with all coded needed to run

---
+++ cudaFastSpeckleGen.py
@@ -0,0 +1,295 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr  4 10:13:29 2024
+
+@author: swordsman
+"""
+
+import numpy as np
+from PIL import Image, ImageSequence
+from scipy.fft import fft2, ifft2, fftshift
+import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
+import os
+from tqdm import tqdm
+import cupy as cp
+from cupyx.scipy.fftpack import get_fft_plan
+from concurrent.futures import ThreadPoolExecutor, as_completed
+import threading
+import gc  # Import garbage collector
+
+# Function to create a detector blur
+def detector_blur(blurx, blury, si):
+    import numpy as np
+    from scipy.fft import fft2, ifft2, fftshift
+    psf = np.zeros((si, si))  # Initialize the point spread function (PSF) array
+    for ii in range(0, blurx - 1):
+        for jj in range(0, blury - 1):
+            ycord = int(np.round(jj + si / 2))
+            xcord = int(np.round(ii + si / 2))
+            psf[ycord][xcord] = 1  # Set the PSF values
+    otf = np.abs(fft2(psf))  # Compute the optical transfer function (OTF) by taking the FFT of the PSF
+    return otf
+
+# Function to create a pupil mask
+def make_pupil(r1, r2, si):
+    import matplotlib.pyplot as plt
+    import numpy as np
+    if 2 * np.floor(si / 2) == si:  # Check if si is even
+        mi = int(np.floor(si / 2))
+        pupilx = np.zeros([si, si])
+        for i in range(0, si - 1):
+            pupilx[i] = range(-mi, mi)
+    if 2 * np.floor(si / 2) != si:  # Check if si is odd
+        mi = int(np.floor(si / 2))
+        pupilx = np.zeros([si, si])
+        for i in range(0, si - 1):
+            pupilx[i] = range(-mi, mi + 1)
+    pupily = np.transpose(pupilx)  # Transpose to get y-coordinates
+    dist2 = np.multiply(pupilx, pupilx) + np.multiply(pupily, pupily)  # Calculate squared distance from center
+    dist = np.sqrt(dist2)  # Calculate distance from center
+    pupil2 = (dist < r1)  # Mask for points within inner radius
+    pupil3 = (dist > r2)  # Mask for points outside outer radius
+    pupil = np.multiply(pupil2.astype(int), pupil3.astype(int))  # Combine masks to create pupil
+    return pupil
+
+# Function to create a complex pupil with focus
+def make_cpupil_focus(r1, r2, si, z, lam, dxx, focal):
+    import numpy as np
+    if 2 * np.floor(si / 2) == si:  # Check if si is even
+        mi = int(np.floor(si / 2))
+        pupilx = np.zeros([si, si])
+        for i in range(0, si - 1):
+            pupilx[i] = range(-mi, mi)
+    if 2 * np.floor(si / 2) != si:  # Check if si is odd
+        mi = int(np.floor(si / 2))
+        pupilx = np.zeros([si, si])
+        for i in range(0, si - 1):
+            pupilx[i] = range(-mi, mi + 1)
+    pupily = np.transpose(pupilx)  # Transpose to get y-coordinates
+    dist2 = np.multiply(pupilx, pupilx) + np.multiply(pupily, pupily)  # Calculate squared distance from center
+    dist = np.sqrt(dist2)  # Calculate distance from center
+    pupil2 = (dist < r1)  # Mask for points within inner radius
+    pupil3 = (dist > r2)  # Mask for points outside outer radius
+    pupil = np.multiply(pupil2.astype(int), pupil3.astype(int))  # Combine masks to create pupil
+    lens_phase = dxx * dxx * np.pi * dist2 / (lam * focal)  # Calculate lens phase
+    phase1 = 2 * np.pi * np.sqrt(dxx * dxx * dist2 + z * z) / lam  # Calculate phase term 1
+    phase2 = 2 * np.pi * np.sqrt(dxx * dxx * dist2 + (focal + .0001) * (focal + .0001)) / lam  # Calculate phase term 2
+    cpupil = np.multiply(np.exp(1j * (phase1 + phase2 - lens_phase)), pupil)  # Create complex pupil
+    return cpupil
+
+# Function to create an optical transfer function (OTF)
+def make_otf2(scale, cpupil):
+    from scipy.fft import fft2
+    import numpy as np
+    psf = fft2(cpupil)  # Compute the point spread function (PSF) by taking the FFT of the complex pupil
+    psf = abs(psf)
+    psf = np.multiply(psf, psf)  # Square the PSF
+    spsf = np.sum(psf)  # Sum of PSF values
+    norm_psf = scale * psf / spsf  # Normalize the PSF
+    otf = fft2(norm_psf)  # Compute the optical transfer function (OTF) by taking the FFT of the normalized PSF
+    return otf
+
+# Function to create a Gaussian beam
+def gaussian_beam(amp, xc, yc, wx, wy, si, dx):
+    # amp is the beam amplitude
+    # xc is the horizontal center of the beam in units of meters
+    # yc is the vertical center of the beam in units of meters 
+    # wx is the horizontal beam waist
+    # wy is the vertical beam waist
+    # si is half the width of the beam array
+    # dx is the sample spacing in the beam array
+    from scipy.fft import fft2
+    import numpy as np
+    beam = np.zeros((2 * si, 2 * si))  # Initialize the beam array
+    for x in range(0, 2 * si):
+        xx = dx * (x - si)
+        for y in range(0, 2 * si):
+            yy = dx * (y - si)
+            beam[x, y] = amp * np.exp(-(xx - xc) * (xx - xc) / wx - (yy - yc) * (yy - yc) / wy) / (np.pi * wx * wy)  # Calculate Gaussian beam values
+    return beam
+
+def generate_speckle_pattern(lam, glass, fft_plan, si, lam_min, increment_scale, pupil, speckles, iter, pbar, lock):
+    lamda = lam / increment_scale * 1e-9 + lam_min  # Calculate the wavelength in meters
+    phase = 2 * np.pi * glass / lamda
+    cpupil = np.multiply(pupil, np.exp((0 + 1j) * phase))
+    #print(f"Generating Speckle Pattern with λ(nm): {round(lamda * 1e9, 2)}")
+    
+    # Convert cpupil to CuPy array for FFT
+    cpupil_cp = cp.asarray(cpupil)
+    
+    # Perform FFT using CuPy with precomputed plan
+    with fft_plan:
+        speckle_field_cp = cp.fft.fftshift(cp.fft.fft2(cp.fft.fftshift(cpupil_cp)))
+    
+    # Convert result back to NumPy array
+    speckle_field = cp.asnumpy(speckle_field_cp)
+    
+    speckle_intensity = np.multiply(np.abs(speckle_field), np.abs(speckle_field))
+    speckles[lam] = speckle_intensity  # Store the speckle intensity in the 2D array
+    current_speckle = (speckles[lam] - np.mean(speckles[lam])) / np.std(speckles[lam])
+    filename = f'speckle_{round(lamda * 1e9)}_phase_{iter}.png'
+
+    if round(lamda * 1e9) == 425 or round(lamda * 1e9) == 625:
+        cropped_speckle = current_speckle[900:1100, 900:1100]  # Crop the speckle pattern to 200x200
+        plt.imshow(cropped_speckle, cmap='magma')
+        plt.title(f'Speckle Pattern at {round(lamda * 1e9)} nm')
+        plt.axis('off')  # Turn off axis labels
+        plt.savefig(os.path.join('speckle_images', filename), bbox_inches='tight', pad_inches=0)
+        plt.close()
+
+        # Perform 2D Fourier Transform on the current speckle pattern
+        fft_speckle = np.abs(fftshift(fft2(fftshift(current_speckle))))
+        fft_slice = fft_speckle[1000]
+        fft_slice_cropped = fft_slice[1100:2000]
+
+        # Plot the Fourier Transform magnitude
+        plt.plot(fft_slice)
+        plt.title(f'Speckle Pattern Intensity (fft_slice) at {round(lamda * 1e9)} nm')
+        plt.xlabel('Index')
+        plt.ylabel('Intensity')
+        plt.savefig(os.path.join('speckle_images', f'speckle_pattern_slice_{round(lamda * 1e9)}_phase_{iter}.png'))
+        plt.close()
+
+        plt.plot(fft_slice_cropped)
+        plt.title(f'Fourier Transform Information (fft_slice_cropped) at {round(lamda * 1e9)} nm')
+        plt.xlabel('Index')
+        plt.ylabel('Intensity')
+        plt.savefig(os.path.join('speckle_images', f'fourier_transform_{round(lamda * 1e9)}_phase_{iter}.png'))
+        plt.close()
+
+    # Update progress bar
+    with lock:
+        pbar.update(1)
+
+def correlate_speckles(speckle_data, iteration_index):
+    num_speckles = speckle_data.shape[1] # change me dumbass
+    print(speckle_data.shape)
+    reference_index = 175
+    reference_speckle = (speckle_data[iteration_index, reference_index] - np.mean(speckle_data[iteration_index, reference_index])) / np.std(speckle_data[iteration_index, reference_index])
+    correlations = np.zeros(num_speckles)
+    wavelengths = np.zeros(num_speckles)
+
+    for lam in range(num_speckles):
+        speckle = (speckle_data[iteration_index, lam] - np.mean(speckle_data[iteration_index, lam])) / np.std(speckle_data[iteration_index, lam])
+        correlations[lam] = np.mean(np.multiply(reference_speckle, speckle))
+        wavelengths[lam] = lam / increment_scale  # Calculate the wavelength increment in nanometers
+
+    wavelengths = wavelengths + 400  # Adjust wavelengths to start from 400 nm
+
+    plt.figure(figsize=(10, 6))
+    plt.plot(wavelengths, correlations, marker='o')
+    plt.title(f'Correlation of Speckle Patterns for Against {reference_index+400} nm')
+    plt.xlabel('Wavelength of Speckle Pattern (nm)')
+    plt.ylabel('Correlation')
+    plt.grid(True)
+    plt.savefig('correlation_plot_generation.png')  # Save the plot to a file
+    plt.close()
+
+pca_components = 144  # Number of PCA components to keep
+# Function to apply PCA to each individual speckle pattern
+def apply_pca_to_speckles(speckles, num_components=pca_components):
+    num_speckles = speckles.shape[0]
+    flattened_size = speckles.shape[1] * speckles.shape[2]
+    
+    pca_speckles = np.zeros((num_speckles, num_components))
+    reshaped_speckles = speckles.reshape(num_speckles, flattened_size)
+    pca = PCA(n_components=num_components)
+    pca_speckles = pca.fit_transform(reshaped_speckles)
+
+    # Reshape the PCA output to match the expected dimensions
+    pca_speckles = pca_speckles.reshape(num_speckles, int(np.sqrt(num_components)), int(np.sqrt(num_components)))
+
+    return pca_speckles
+
+
+si = 2000  # Size of the image
+fl = .1  # Focal length of the lens
+D = .01  # Diameter of the lens
+lam_min = 400e-9  # Minimum wavelength
+lam_max = 700e-9  # Maximum wavelength
+infield = gaussian_beam(1, 0, 0, .002, .002, 1000, 1e-5)
+
+# #Label the graph
+# plt.title("Gaussian Beam")
+# plt.xlabel("x(um)")
+# plt.ylabel("y(um)")
+# plt.imshow(infield, cmap='gray')
+# plt.show()
+#plt.imsave(f'gaussian_beam.png', infield, cmap='gray')  # Create the input field using az Gaussian beam
+
+numIterations = 1  # Number of iterations
+numSpeckles = 256  # Total number of speckle patterns to generate per iteration
+increment_scale = 1  # Inverse Scale factor to increment the wavelength by, 1 is 1nm, 4 is 0.25nm
+
+#make a directory to store speckle images, move on if it already exists
+if not os.path.exists('speckle_images'):
+    os.makedirs('speckle_images')
+# Initialize the array to store speckle images.
+# The first dimension isolates speckle patterns to use the same phase screen
+# The second dimension for the different wavelengths of speckle patterns for a given phase screen
+# The third and fourth dimensions are the image data itself, assuming after PCA
+speckle_imgs = np.zeros((numIterations, numSpeckles, int(np.sqrt(pca_components)), int(np.sqrt(pca_components)))) 
+
+print("Generating speckle patterns...")
+print(f"Number of speckle patterns: {numSpeckles}, Increment: {1/increment_scale} nm")
+
+# Precompute the FFT plan for CuPy
+fft_plan = get_fft_plan(cp.zeros((si, si), dtype=cp.complex128))
+
+for iter in range(numIterations):
+    # Generate a new random phase screen
+    #print("Generating speckles on iteration: ", iter)
+    glass = 0.4 * np.random.uniform(0, .001, (si, si))  # Generate a random phase screen from a uniform distribution. 0.4 represent the index of refraction of the glass and the air
+    speckles = np.zeros((numSpeckles, si, si))  # Initialize a temporary array to store speckle patterns
+
+    lock = threading.Lock()
+    with ThreadPoolExecutor() as executor:
+        with tqdm(total=numSpeckles, desc=f"Processing speckle patterns on iteration {iter}") as pbar:
+            futures = [executor.submit(generate_speckle_pattern, lam, glass, fft_plan, si, lam_min, increment_scale, make_pupil(500 * lam_min / (lam / increment_scale * 1e-9 + lam_min), 0, si), speckles, iter, pbar, lock) for lam in range(numSpeckles)]
+            for future in as_completed(futures):
+                future.result()
+
+    print(f"Speckle pattern {iter} generated. Converting to PCA format...")
+    # Convert the speckle pattern to PCA format and store it to temporary array of PCA transformed images
+
+    #correlate_speckles(speckles, 0) change index on shape dumbass
+
+    pca_speckles = apply_pca_to_speckles(speckles)
+    speckle_imgs[iter] = pca_speckles
+    print(f"Speckle pattern {iter} converted to PCA format.")
+    #correlate_speckles(speckles, 0)
+    # Clear variables and run garbage collector
+    del speckles, pca_speckles
+    gc.collect()
+
+#print the storage size of the speckle images array in a nicely formatted way
+print(f"Speckle Images Array Shape: {speckle_imgs.shape}")
+print(f"Speckle Images Array Size: {speckle_imgs.nbytes / 1e6} MB")
+
+# Save the speckle_imgs variable to a file including the shape
+np.save('speckle_imgs.npy', speckle_imgs)
+
+# Example usage
+correlate_speckles(speckle_imgs, 0)
+
+# # Apply PCA to each individual speckle pattern
+# pca_speckle_imgs = apply_pca_to_speckles(speckle_imgs)
+# print(f"PCA Speckle Images Array Size: {pca_speckle_imgs.nbytes / 1e6} MB")
+
+# # replace the raw speckle images with the PCA transformed images
+# speckle_imgs = pca_speckle_imgs
+# print(speckle_imgs.shape)
+
+# # Example usage: Plot the first PCA transformed speckle pattern
+# plt.imshow(pca_speckle_imgs[0, 0].reshape(8, 8), cmap='gray')
+# plt.title('PCA Transformed Speckle Pattern')
+# plt.xlabel('x(um)')
+# plt.ylabel('y(um)')
+# #plt.show()
+
+# correlate_speckles(pca_speckle_imgs, 0)
+
+print("Done")
+
+
---
+++ speckleCNN.py
@@ -0,0 +1,316 @@
+import numpy as np
+from sklearn.model_selection import StratifiedShuffleSplit
+from tensorflow.keras.models import Sequential, load_model
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Input, BatchNormalization, Dropout
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.losses import MeanSquaredError, MeanAbsoluteError
+from tensorflow.keras.callbacks import ReduceLROnPlateau
+import matplotlib.pyplot as plt
+import sklearn.metrics
+import itertools
+import os
+from matplotlib.colors import LogNorm  # Add this import
+
+# Load the speckle images
+speckle_imgs = np.load('./FINALspeckle_imgs.npy')  # pwd should look like "/home/aaron/Documents/AFIT/SpeckleResearch"
+
+# Flag to run a correlation test to see what the baseline looks like
+run_correlation_test = False
+if run_correlation_test:
+    known_data = speckle_imgs[:19]
+    num_iterations = known_data.shape[0]
+    num_speckles = known_data.shape[1]
+    sweep_correlations = np.zeros(num_speckles)
+    for unkn_idx in range(num_speckles):
+        known_data = speckle_imgs[:19]
+        unknown_speckle = (speckle_imgs[19, unkn_idx] - np.mean(speckle_imgs[19, unkn_idx])) / np.std(speckle_imgs[19, unkn_idx])
+
+        summed_correlations = np.zeros(num_speckles)
+        wavelengths = np.zeros(num_speckles)
+
+        for iter_idx in range(num_iterations):
+            correlations = np.zeros(num_speckles)
+            for lam in range(num_speckles):
+                speckle = (known_data[iter_idx, lam] - np.mean(known_data[iter_idx, lam])) / np.std(known_data[iter_idx, lam])
+                correlations[lam] = np.mean(np.multiply(unknown_speckle, speckle))
+
+            summed_correlations += correlations
+
+        sweep_correlations[unkn_idx] = np.argmax(summed_correlations)
+
+    wavelengths = np.arange(num_speckles) + 400
+    sweep_correlations = sweep_correlations + 400
+
+    plt.figure(figsize=(10, 6))
+    plt.plot(wavelengths, sweep_correlations)
+    plt.title(f'Confusion Matrix for Speckle Pattern Correlation Test')
+    plt.xlabel('Wavelength of Unknown Speckle Pattern (nm)')
+    plt.ylabel('Highest Correlation Value')
+    plt.gca().invert_yaxis()  # Invert the y-axis
+    plt.grid(True)
+    plt.savefig('confusion_matrix_correlation_test.png')  # Save the plot to a file
+    plt.close()
+
+# Get the shape of the data
+num_iterations, num_speckles, img_height, img_width = speckle_imgs.shape
+
+# Flatten the data structure
+X = speckle_imgs.reshape(-1, img_height, img_width, 1)  # Add a channel dimension for CNN
+
+# Normalize the data to the range [0, 1]
+X = X / 255.0
+
+# Create labels based on the second dimension index
+wavelengths = np.arange(0, 256)  # Wavelengths from 0 to 255
+labels = np.tile(wavelengths, num_iterations)  # Repeat for each iteration
+
+# Create a stratified split
+sss = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
+train_index, temp_index = next(sss.split(X, labels))
+
+X_train, X_temp = X[train_index], X[temp_index]
+y_train, y_temp = labels[train_index], labels[temp_index]
+
+# Further split the temp set into validation and test sets
+sss = StratifiedShuffleSplit(n_splits=1, test_size=0.5, random_state=42)
+val_index, test_index = next(sss.split(X_temp, y_temp))
+
+X_val, X_test = X_temp[val_index], X_temp[test_index]
+y_val, y_test = y_temp[val_index], y_temp[test_index]
+
+# Print the shape of the training, validation, and testing sets to verify
+print(f'Training data shape: {X_train.shape}')
+print(f'Training labels shape: {y_train.shape}')
+print(f'Validation data shape: {X_val.shape}')
+print(f'Validation labels shape: {y_val.shape}')
+print(f'Testing data shape: {X_test.shape}')
+print(f'Testing labels shape: {y_test.shape}')
+
+# Function to build the model
+def build_model(model_flag):
+    if model_flag == 1:  # basic
+        model = Sequential([
+            Input(shape=(img_height, img_width, 1)),
+            Conv2D(32, (3, 3), activation='relu', padding='same'),
+            MaxPooling2D((2, 2)),
+            Conv2D(64, (3, 3), activation='relu', padding='same'),
+            MaxPooling2D((2, 2)),
+            Conv2D(128, (3, 3), activation='relu', padding='same'),
+            Flatten(),
+            Dense(128, activation='relu'),
+            Dense(1)
+        ])
+    elif model_flag == 2:  # overfit
+        model = Sequential([
+            Input(shape=(img_height, img_width, 1)),
+            Conv2D(64, (3, 3), activation='relu', padding='same'),
+            Conv2D(64, (3, 3), activation='relu', padding='same'),
+            MaxPooling2D((2, 2)),
+            Conv2D(128, (3, 3), activation='relu', padding='same'),
+            Conv2D(128, (3, 3), activation='relu', padding='same'),
+            MaxPooling2D((2, 2)),
+            Conv2D(256, (3, 3), activation='relu', padding='same'),
+            Conv2D(256, (3, 3), activation='relu', padding='same'),
+            Flatten(),
+            Dense(512, activation='relu'),
+            Dense(512, activation='relu'),
+            Dense(1)
+        ])
+    elif model_flag == 3:  # regularized
+        model = Sequential([
+            Input(shape=(img_height, img_width, 1)),
+            Conv2D(32, (3, 3), activation='relu', padding='same'),
+            BatchNormalization(),
+            MaxPooling2D((2, 2)),
+            Dropout(0.25),
+            Conv2D(64, (3, 3), activation='relu', padding='same'),
+            BatchNormalization(),
+            MaxPooling2D((2, 2)),
+            Dropout(0.25),
+            Conv2D(128, (3, 3), activation='relu', padding='same'),
+            BatchNormalization(),
+            Flatten(),
+            Dense(128, activation='relu'),
+            Dropout(0.5),
+            Dense(1)
+        ])
+    else:
+        raise ValueError("Invalid model_flag value. Choose 1, 2 or 3 for basic, overfit, or regularized.")
+    return model
+
+# Flag to use a saved model or train a new one
+use_saved_model = False  # Set to False to train a new model
+
+if use_saved_model and os.path.exists('best_model_overfit4.h5'):
+    # Load the saved model
+    best_model = load_model('best_model_overfit4.h5', compile=False)
+    best_model.compile(optimizer=Adam(learning_rate=0.001), loss=MeanSquaredError(), metrics=[MeanAbsoluteError()])
+    print('Loaded saved model.')
+    
+else:
+    # Initialize variables to keep track of the best model
+    best_model = None
+    best_val_mae = float('inf')
+
+    # Store the training history of each model
+    histories = []
+
+    # Train multiple models and keep track of the best one
+    num_models = 10
+    for i in range(num_models):
+        print(f'Training model {i+1}/{num_models}')
+        
+        # Build the model
+        model = build_model(model_flag=3)  # Change model_flag to 2 for overfit model or 3 for regularized model
+        
+        # Compile the model
+        model.compile(optimizer=Adam(learning_rate=0.001), loss=MeanSquaredError(), metrics=[MeanAbsoluteError()])
+        
+        # Callback for learning rate reduction
+        reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.2, patience=5, min_lr=0.00001)
+        
+        # Train the model
+        history = model.fit(X_train, y_train, batch_size=32, epochs=200, validation_data=(X_val, y_val),
+                            callbacks=[reduce_lr])
+        
+        # Store the training history
+        histories.append(history)
+        
+        # Evaluate the model on the validation set
+        val_loss, val_mae = model.evaluate(X_val, y_val)
+        print(f'Validation MAE: {val_mae}')
+        
+        # Update the best model if the current model is better
+        if val_mae < best_val_mae:
+            best_val_mae = val_mae
+            best_model = model
+
+    # Save the best model
+    best_model.save('best_model.h5')
+    print(f'Best model saved with validation MAE: {best_val_mae}')
+
+# Evaluate the model on the test set
+test_loss, test_mae = best_model.evaluate(X_test, y_test)
+print(f'Test MAE: {test_mae}')
+print(f'Test Loss: {test_loss}')
+
+# Evaluate the model on the training set
+train_loss, train_mae = best_model.evaluate(X_train, y_train)
+print(f'Training MAE: {train_mae}')
+print(f'Training Loss: {train_loss}')
+
+# Evaluate the model on the validation set
+val_loss, val_mae = best_model.evaluate(X_val, y_val)
+print(f'Validation MAE: {val_mae}')
+print(f'Validation Loss: {val_loss}')
+
+# Print the model summary
+#best_model.summary()
+
+if not use_saved_model:
+    # Plot the training progress of all models
+    plt.figure(figsize=(12, 8))
+    colors = plt.get_cmap('tab10', num_models)  # Get a colormap with unique colors
+
+    for i, history in enumerate(histories):
+        #plt.plot(history.history['mean_absolute_error'], color=colors(i), linestyle='-.', label=f'Model {i+1} Training MAE')
+        plt.plot(history.history['val_mean_absolute_error'], color=colors(i), linestyle='-', label=f'Model {i+1} Validation MAE')
+
+    plt.yscale('log')  # Set y-axis to logarithmic scale
+    plt.title('Validation MAE for of Regularized Models')
+    plt.xlabel('Epoch')
+    plt.ylabel('Validation MAE (log scale)')
+    plt.legend()
+    plt.grid(True)
+    plt.savefig('training_progress_all_models_regularized4.png')
+    plt.close()
+
+    # Plot the validation MAE and training MAE of the best model
+    best_model_index = np.argmin([h.history['val_mean_absolute_error'][-1] for h in histories])
+    best_history = histories[best_model_index]
+
+    plt.figure(figsize=(12, 8))
+    plt.plot(best_history.history['val_mean_absolute_error'], label='Validation MAE of Best Model', linestyle='-')
+    plt.plot(best_history.history['mean_absolute_error'], label='Training MAE of Best Model', linestyle='--')
+    #plt.yscale('log')  # Set y-axis to logarithmic scale
+    plt.title('Validation MAE and Training MAE of Best Regularized Model')
+    plt.xlabel('Epoch')
+    plt.ylabel('MAE (log scale)')
+    plt.legend()
+    plt.grid(True)
+    plt.savefig('best_model_validation_training_mae_regularized4_temp.png')
+    plt.close()
+
+# Make predictions
+predictions = best_model.predict(X_test)
+
+# Print the first 10 sets of predictions and truths
+for i in range(10):
+    print(f'Prediction: {predictions[i]+400}, Truth: [{y_test[i]+400}]') # Add 400 to account for real life labels
+
+# Make predictions for the confusion matrix
+y_pred = np.round(best_model.predict(X_test)).astype(int)
+
+# Ensure predictions are within the valid range
+y_pred = np.clip(y_pred, 0, 255)
+
+# Define class names and figure path
+class_names = [str(i) for i in range(256)]
+fig_path = './figures'
+
+# Create the directory if it doesn't exist
+os.makedirs(fig_path, exist_ok=True)
+
+# Create confusion matrix
+confusion_matrix = sklearn.metrics.confusion_matrix(y_true=y_test, y_pred=y_pred)
+
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap='Greys',
+                          label_interval=25,
+                          figsize=(20, 20),
+                          title_fontsize=36,
+                          label_fontsize=30,
+                          tick_fontsize=24,
+                          colorbar_fontsize=24,
+                          test_mae=None):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+    
+    # Increment all values by 400 to account for real life labels
+    classes = [str(int(cls) + 400) for cls in classes]
+    
+    plt.figure(figsize=figsize)
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title, fontsize=title_fontsize)
+    cbar = plt.colorbar()
+    cbar.ax.tick_params(labelsize=colorbar_fontsize)
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks[::label_interval], classes[::label_interval], rotation=45, fontsize=tick_fontsize)
+    plt.yticks(tick_marks[::label_interval], classes[::label_interval], fontsize=tick_fontsize)
+
+    plt.ylabel('True label (nm)', fontsize=label_fontsize)
+    plt.xlabel('Predicted label (nm)', fontsize=label_fontsize)
+    plt.grid(False)  # Remove grid lines
+
+    # Add Test MAE and Test Loss annotation in the upper right corner
+    if test_mae is not None and test_loss is not None:
+        plt.text(len(classes) - 1, -10, f'Test MAE: {test_mae:.4f}\nTest Loss: {test_loss:.4f}', fontsize=label_fontsize, ha='right')
+    elif test_mae is not None:
+        plt.text(len(classes) - 1, -10, f'Test MAE: {test_mae:.4f}', fontsize=label_fontsize, ha='right')
+    elif test_loss is not None:
+        plt.text(len(classes) - 1, -10, f'Test Loss: {test_loss:.4f}', fontsize=label_fontsize, ha='right')
+
+    plt.tight_layout(pad=2.0)  # Adjust padding to ensure labels are not cut off
+
+# Plot normalized confusion matrix
+plot_confusion_matrix(confusion_matrix, classes=class_names, normalize=True,
+                      title='Normalized confusion matrix', test_mae=test_mae)
+
+plt.savefig(f'{fig_path}/normalized_confusion_matrix_temp.png')
+# plt.show()
